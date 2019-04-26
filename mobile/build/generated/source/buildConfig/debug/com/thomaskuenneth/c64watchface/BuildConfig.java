c028a9666b22e6f37ea98a94dc7f80cbaa7f4a91
heikobremer <hbremer@techfak.unci-bielefeld.de>
2019-04-26 21:35:42 +0200

    Initial commit

---
+++ mobile/build/generated/source/buildConfig/debug/com/thomaskuenneth/c64watchface/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.thomaskuenneth.c64watchface;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.thomaskuenneth.c64watchface";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 13202;
+  public static final String VERSION_NAME = "1.3.2";
+}
